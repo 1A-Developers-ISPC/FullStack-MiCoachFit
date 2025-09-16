ac040552e12edacbcfa3f53bfbe25f5c58cd1359
Eric-Heredia <ericvheredia@gmail.com>
2025-09-15 23:19:02 -0300

    feat: impletanción de registro de ususario

--- back/database.py
+++ back/database.py
@@ -34,7 +34,28 @@ class ConexionBD:
             self.connection.close()
             print("\n🔒 Conexión a la base de datos cerrada.")
 
+    
     def buscar_usuario(self, nombre_usuario):
         query = "SELECT u.*, r.nombre_rol FROM usuarios u JOIN roles r ON u.id_rol = r.id_rol WHERE u.nombre_usuario = %s"
         self.cursor.execute(query, (nombre_usuario,))
         return self.cursor.fetchone()
+    
+    
+    def registrar_usuario(self, nombre, apellido, email, nombre_usuario, telefono, contrasena, rol):
+        try:
+            self.cursor.execute("SELECT id_rol FROM roles WHERE nombre_rol = %s", (rol,))
+            rol_id = self.cursor.fetchone()
+            if not rol_id:
+                print(f"\n❌ Rol '{rol}' no encontrado en la base de datos.")
+                return False
+
+            query = "INSERT INTO usuarios (nombre, apellido, email, nombre_usuario, telefono, password, id_rol) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            values = (nombre, apellido, email, nombre_usuario, telefono, contrasena, rol_id['id_rol'])
+            self.cursor.execute(query, values)
+            self.connection.commit()
+            return True
+        except Error as e:
+            print(f"\n❌ Error al registrar el usuario: {e}")
+            self.connection.rollback()
+            return False
+
